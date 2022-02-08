af158954ccc42b09a9d2c9fc99f2eaa3e8b96530
ko-to-mi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-02-08 09:31:44 +0900

    削除機能

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,18 @@ export const App = () => {
   const [completeTodos, setCompleteTodos] = useState(["ううううう"]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
+
   const onClickAdd = () => {
     if (todoText === "") return;
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
     setTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
   };
 
   return (
@@ -30,12 +37,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
